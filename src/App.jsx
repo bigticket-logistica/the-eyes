2576a261d28e127c807e20fa278823aa7dab19ac
bigticket-logistica <camilonaranjo.bigticket@gmail.com>
2026-08-06 17:25:17 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Directorio from "./modulos/Directorio.jsx";
 import Bitacora from "./modulos/Bitacora.jsx";
 import Correos from "./modulos/Correos.jsx";
 import Mensajes from "./modulos/Mensajes.jsx";
+import Anomalias from "./modulos/Anomalias.jsx";
 import Topbar from "./componentes/Topbar.jsx";
 import { AlertasProvider, ContenedorToasts } from "./shared/alertas.jsx";
 
@@ -62,6 +63,7 @@ export default function App() {
             <Route path="/historico" element={<Historico />} />
             <Route path="/consultas" element={<Consultas />} />
             <Route path="/mensajes" element={<Mensajes />} />
+            <Route path="/anomalias" element={<Anomalias />} />
             <Route path="/login" element={<Navigate to="/" replace />} />
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
